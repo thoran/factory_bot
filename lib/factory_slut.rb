29bf3f2e65ddbe0068b17058b20349ad83e26cf4
thoran <code@thoran.com>
2017-10-27 01:58:28 +1100

    A little light relief for those on the other side of the renaming farce.

---
+++ lib/factory_slut.rb
@@ -0,0 +1,5 @@
+require "factory_bot"
+
+FactorySlut = FactoryBot
+
+warn "The factory_girl gem has been deprecated and has been replaced by factory_bot because some people have got nothing better to do than worry about being offended at everything."
